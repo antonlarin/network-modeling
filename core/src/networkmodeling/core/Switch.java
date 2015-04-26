210140a182b156c9f6edd6033359f3509b84c408
Anton Larin <edding.default@gmail.com>
2015-04-26 15:05:38 +0300

    Add network switch

---
+++ core/src/networkmodeling/core/Switch.java
@@ -0,0 +1,41 @@
+package networkmodeling.core;
+
+import java.util.HashMap;
+
+public class Switch extends NetworkDevice {
+
+    public Switch(MacAddress macAddress, int portCount) {
+        super(macAddress, portCount);
+
+        camTable = new HashMap<>();
+    }
+
+    @Override
+    public void handleIncomingFrame(Frame frame, Port receivingPort) {
+        camTable.put(frame.getSenderMac(), receivingPort);
+
+        Port outputPort = camTable.get(frame.getTargetMac());
+        if (outputPort == null) {
+            broadcastFrame(frame, receivingPort);
+        } else {
+            try {
+                outputPort.sendFrame(frame);
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    private void broadcastFrame(Frame frame, Port receivingPort) {
+        for (Port port : getPorts()) {
+            if (port != receivingPort) {
+                try {
+                    port.sendFrame(frame);
+                } catch (Exception ex) {
+                }
+            }
+        }
+    }
+
+
+    private final HashMap<MacAddress, Port> camTable;
+}
