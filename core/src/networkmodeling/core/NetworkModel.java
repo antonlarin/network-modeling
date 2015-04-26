14117de0d50bd42e52b443b6795040827de9cb8e
Sovrasov Vlad <vlads247@gmail.com>
2015-04-26 13:01:19 +0300

    Add basic methods of NetworkModel.

--- core/src/networkmodeling/core/NetworkModel.java
+++ core/src/networkmodeling/core/NetworkModel.java
@@ -1,5 +1,47 @@
 package networkmodeling.core;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class NetworkModel {
     
+    public NetworkModel()
+    {
+        devices = new LinkedList<>();
+    }
+    
+    void AddDevice(NetworkDevice dev)
+    {
+        devices.add(dev);
+    }
+    
+    void ConnectDevices(NetworkDevice dev1, NetworkDevice dev2) throws Exception
+    {
+        dev1.connectTo(dev2);
+    }
+    
+    void DisconnectDevices(NetworkDevice dev1, NetworkDevice dev2)
+    {
+        
+    }
+    
+    void DeleteDevice(NetworkDevice dev)
+    {
+        Iterator<NetworkDevice> i = devices.iterator();
+        while(i.hasNext())
+        {
+            if(i.next().equals(dev))
+            {
+                i.remove();
+                break;
+            }
+        }
+    }
+    
+    LinkedList<NetworkDevice> getDevicesList()
+    {
+        return devices;
+    }
+    
+    private final LinkedList<NetworkDevice> devices;
 }
